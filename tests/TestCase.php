40ff0faa809104220d1670a938bfbb8bd7040b80
Vasily Belosludcev <bupy765@gmail.com>
2017-09-18 11:36:00 +0500

    added docker, fixed tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,13 +2,13 @@
 
 namespace bupy7\xml\constructor\tests;
 
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
 /**
  * @author Vaseelie Belosloodcev <https://github.com/bupy7>
  * @since 1.2.2
  */
-class TestCase extends PHPUnit_Framework_TestCase
+class TestCase extends BaseTestCase
 {
 
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../src/XmlConstructor.php';
+if (!class_exists('PHPUnit_Framework_TestCase', false)) {
+    class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+}
+
+require __DIR__ . '/../vendor/autoload.php';
