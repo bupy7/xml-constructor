19d3cb940259ed9949735a6a7d7f65c82efac3f7
Vasily Belosludcev <bupy765@gmail.com>
2017-09-18 11:57:22 +0500

    fixed versions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,3 @@
 <?php
 
-if (!class_exists('PHPUnit_Framework_TestCase', false)) {
-    class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
-}
-
 require __DIR__ . '/../vendor/autoload.php';
